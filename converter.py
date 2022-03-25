7b9a67a937045cb5919c3173c9c88f5629a932ef
Federico Gabusi <gabusi@mobisec.com>
2022-03-25 17:06:22 +0100

    Cleaned a bit

--- converter.py
+++ converter.py
@@ -2,14 +2,9 @@ import requests
 import sys
 import json
 
-
 args = sys.argv[1:]
 api_key = "71409165cf9d683741c6c9c7"
 request = requests.get('https://v6.exchangerate-api.com/v6' + '/' + api_key + '/'+'pair'+'/' + args[0] + '/' + args[1] + '/' + args[2])
-#base_code
-#target_code
-#conversion_rate
-#conversion_result
 json = request.json()
 print(json['base_code'],json['target_code'],json['conversion_rate'],json['conversion_result'])
 
